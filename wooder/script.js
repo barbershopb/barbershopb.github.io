a5ed7b24d527f7021d1b49963906899b7cb53aa8
barbershopb <54004998+barbershopb@users.noreply.github.com>
2019-08-23 17:58:48 +0300

    Add files via upload

--- wooder/script.js
+++ wooder/script.js
@@ -1,1 +1,24 @@
-1
+function videoUnavailableDesign() {
+	var videoMessageShow = document.getElementById("js-unavailable__design");
+	videoMessageShow.classList.add("video-show__design");
+}
+
+function videoUnavailableSkills() {
+	var videoMessageShow = document.getElementById("js-unavailable__skills");
+	videoMessageShow.classList.add("video-show__skills");
+}
+
+function videoUnavailableQuality() {
+	var videoMessageShow = document.getElementById("js-unavailable__quality");
+	videoMessageShow.classList.add("video-show__quality");
+}
+
+function videoSkills() {
+	var videoShow = document.getElementById("js-skills__video");
+	videoShow.classList.add("skills__video-open");
+}
+
+
+
+
+
